9b13ec9747dcf025b0be2c3bb3500c2e8411da0f
Anantjit <anantjitchander@gmail.com>
2021-03-20 13:39:34 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,20 +1,20 @@
-prediction_1="";
-prediction_2="";
+prediction_1 = "";
+prediction_2 = "";
 
 Webcam.set({
-    width:350,
-    height:300,
-    image_format:'png',
-    png_quality:90
+    width: 350,
+    height: 300,
+    image_format: 'png',
+    png_quality: 90
 });
 
-camera=document.getElementById("camera");
+camera = document.getElementById("camera");
 
 Webcam.attach('#camera');
 
-function take_snapshot(){
-    webcam.snap(function (data_uri){
-        document.getElementById("result").innerHTML='<img src="'+data_uri+'" id="captured_image"/>';
+function take_snapshot() {
+    Webcam.snap(function (data_uri) {
+        document.getElementById("result").innerHTML = '<img src="' + data_uri + '" id="captured_image"/>';
     });
 }
 
@@ -22,14 +22,58 @@ console.log('ml5 version', ml5.version);
 
 classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/5mxzZHkpx/model.json', modelLoaded);
 
-function modelLoaded(){
-    console.log(modelLoaded);
+function modelLoaded() {
+    console.log("modelLoaded");
 }
 
-function speak(){
-    var synth=window.SpeachSynthesis;
-    speak_data_1="The first perdiction is"+prediction_1;
-    speak_data_2="The second prediction is"+prediction_2;
-    var utterThis=new SpeachSynthesisIsUtterence(speak_data_1 + speak_data_2);
+function speak() {
+    var synth = window.speechSynthesis;
+    speak_data_1 = "The first perdiction is" + prediction_1;
+    speak_data_2 = "The second prediction is" + prediction_2;
+    var utterThis = new SpeechSynthesisIsUtterance(speak_data_1 + speak_data_2);
     synth.speak(utterThis);
+}
+
+function check() {
+    img = document.getElementById('captured_image');
+    classifier.classify(img, gotResult);
+}
+
+function gotResult(error, results) {
+    if (error) {
+        console.log(error);
+        console.error(error);
+    } else {
+        console.log(results);
+        document.getElementById("result_emotion_name").innerHTML = results[0].label;
+        document.getElementById("result_emotion_name").innerHTML = results[1].label;
+
+        prediction_1 = results[0].label;
+        prediction_2 = results[1].label;
+        speak();
+
+        if (results[0].label == "happy") {
+            document.getElementById("update_emoji").innerHTML = "&#128552"
+        }
+
+        if (results[0] == "sad") {
+            document.getElementById("update_emoji").innerHTML = "&#128532";
+        }
+
+        if (results[0] == "angry") {
+            document.getElementById("update_emoji").innerHTML = "&#128548";
+        }
+
+        if (results[1] == "happy") {
+            document.getElementById("update_emoji2").innerHTML = "&#128552";
+        }
+
+        if (results[1] == "sad") {
+            document.getElementById("update_emoji2").innerHTML = "&#128532";
+        }
+
+        if (results[1] == "angry") {
+            document.getElementById("update_emoji2").innerHTML = "&#128548";
+        }
+    }
 }
